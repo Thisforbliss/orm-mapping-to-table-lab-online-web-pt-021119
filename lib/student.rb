335035830286dd72f4e7d5b9278d43e0764b8d5d
Malcolm Leake <echelon1089@gmail.com>
2019-05-09 22:54:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,8 @@ class Student
     INSERT INTO students (name, grade)
     VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.name, self.grade) 
+    @id = 
   end
   
   
